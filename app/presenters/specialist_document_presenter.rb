df6c1813c881079ddce9506e5eb930d229436d7c
Andrew Hilton <andrew.hilton@digital.cabinet-office.gov.uk>
2017-01-23 13:58:48 +0000

    get the footer rendering correctly
    parse all dates in the metadata
    with this, on the aaib template, date of occurence was on the left
    as opposed to just metadata, not too sure how to do this cleanly
    shoudl all dates belong on the left hand side? also the dates
    came through fromt he content api in a weird format, made human
    readable, but the wrong format
    
    the headers where dealt with differently in specialist frontend
    as the headers where available int he content store, it either uses
    the current partial and resepnter or uses the content item headins

--- app/presenters/specialist_document_presenter.rb
+++ app/presenters/specialist_document_presenter.rb
@@ -12,15 +12,58 @@ class SpecialistDocumentPresenter < ContentItemPresenter
   def metadata
     super.tap do |md|
       metadata = content_item["details"]["metadata"].reject{ |m| m == "document_type" }
+      metadata.each do |key,value|
+        date = Date.parse(value) rescue nil
+        metadata[key] = nice_date_format(date) if date
+      end
+
       metadata = metadata.transform_keys{ |md| md.to_s.humanize }
       md[:other].merge!(metadata)
       return md
     end
   end
 
+  # this seems to be legacy now, as it comes back with headings
   def contents
     extract_headings_with_ids(body).map do |heading|
       link_to(heading[:text], "##{heading[:id]}")
+    end
+  end
+
+  def change_history
+    content_item["details"]["change_history"] || []
+  end
+
+  def headings
+    content_item["details"]["headers"]
+  end
+
+
+  # copied from specialist document
+  def nice_date_format(time_string)
+    unless time_string.blank?
+      time = Time.zone.parse(time_string.to_s)
+      string = "<time datetime='#{time.iso8601}'>"
+      string += time.strftime('%-d %B %Y')
+      string += '</time>'
+      string.html_safe
+    end
+  end
+
+  def first_edition?
+    change_history.size <= 1
+  end
+
+  def bulk_published
+    content_item["details"]["metadata"]["bulk_published"]
+  end
+
+  def footer_date_metadata
+    return {} if bulk_published
+    if first_edition?
+      { published: nice_date_format(published) }
+    else
+      { updated: nice_date_format(updated) }
     end
   end
 end
