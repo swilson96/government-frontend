38fa276dd91cddde19a0e77462da1ad62b23079d
Andrew Hilton <igberty@gmail.com>
2017-02-03 12:43:53 +0000

    fix routes clash with foreign travel advice

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   mount JasmineRails::Engine => "/specs" if defined?(JasmineRails)
 
   with_options format: false do |r|
-    r.get 'healthcheck', to: proc { [200, {}, ['']] }
-    r.get ':content_item_prepend/:path/:section' => 'content_items#show'
+ r.get 'healthcheck', to: proc { [200, {}, ['']] }
+    r.get 'foreign-travel-advice/:path/:section' => 'content_items#show',
+          content_item_prepend: "foreign-travel-advice/", as: :travel_advice_country_show
+    r.get 'foreign-travel-advice/:path' => 'content_items#show',
+          content_item_prepend: "foreign-travel-advice/", as: :travel_advice_country_index
     r.get ':path/:section' => 'content_items#show'
     r.get '*path' => 'content_items#show', constraints: { path: %r[.*] }
   end
