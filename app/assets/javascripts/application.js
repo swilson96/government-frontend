eeae1ea203ec2d3e43b5f14dd85b94fae669ea81
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2017-11-13 13:35:37 +0000

    Add tab javascript/css
    This would probably need rewriting?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,12 @@
+//= require_tree ./vendor
 //= require_tree ./modules
+
+var $document = $(document);
+
+$document.ready(function () {
+  if ($document.find('.js-tabs').length) {
+    $document.tabs({
+      scrollOnload: true
+    });
+  }
+});
