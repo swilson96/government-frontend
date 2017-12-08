767b02cb00920ae89cc0b16622a03fbea3cc75c1
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-12-08 15:43:42 +0000

    Fix no index integration test

--- test/integration/service_sign_in_test.rb
+++ test/integration/service_sign_in_test.rb
@@ -9,7 +9,7 @@ class ServiceSignInTest < ActionDispatch::IntegrationTest
     setup_and_visit_content_item('service_sign_in')
 
     assert page.has_css?("title", text: "Prove your identity to continue - GOV.UK")
-    assert page.has_css?('meta[name="robots"][content="noindex, nofollow"]')
+    assert page.has_css?('meta[name="robots"][content="noindex, nofollow"]', visible: false)
     refute page.has_css?(shared_component_selector('breadcrumbs'))
     refute page.has_css?(shared_component_selector('government_navigation'))
 
